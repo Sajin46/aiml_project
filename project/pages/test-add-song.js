7ed3519d8b0d625b6c43281a97b58d17464f456e
Sajin S <154986500+Sajin46@users.noreply.github.com>
2025-12-10 16:00:56 +0530

    Add files via upload

---
+++ project/pages/test-add-song.js
@@ -0,0 +1,10 @@
+// project/pages/test-add-song.js
+
+export default function TestAddSong() {
+  return (
+    <div>
+      <h1>Add Song Test Page</h1>
+      {/* You can put your add-song form here */}
+    </div>
+  );
+}
